e8313072824f52669ad285bc63b899ab535376bf
FADI <fahadkiyani28@gmail.com>
2025-10-25 04:40:53 +0500

    js

---
+++ Variables.js
@@ -0,0 +1,12 @@
+const Name = "Fahad";
+let age = "26";
+var Designation = "Developer";
+weight = "90kg";
+let account;
+
+//  Name = "Fahad Azhar";  const cannot be changed
+age = "27";
+Designation = "Full Stack Developer";
+weight = "78kg";
+
+console.table([Name, age, Designation, weight, account]);
---
+++ dataTypes.js
@@ -0,0 +1,2 @@
+"use strict"; //treat all js code as  newer version
+// alert("Hello"); we are using node js not browser
