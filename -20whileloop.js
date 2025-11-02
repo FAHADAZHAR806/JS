f7d23eefe4e10bed64c520b966f9547140b037c7
FADI <fahadkiyani28@gmail.com>
2025-11-02 20:47:59 +0500

    dowhile,while

---
+++ -20whileloop.js
@@ -0,0 +1,15 @@
+let i = 1;
+while (i <= 10) {
+  table = 2 * [i];
+  console.log(table);
+  i++;
+}
+
+let arry = ["Flash", "Captain", "Tony"];
+let j = 0;
+do {
+  console.log(arry[j]);
+  j++;
+} while (j < arry.length);
+
+// do while loop executes first time whether condition met or not met
