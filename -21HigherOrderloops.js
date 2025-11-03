419848401822cd1e4450fa0ec440dca8fc4ea15a
FADI <fahadkiyani28@gmail.com>
2025-11-03 11:16:10 +0500

    foreach loop , for of , for in

--- -21HigherOrderloops.js
+++ -21HigherOrderloops.js
@@ -1,1 +1,24 @@
 // for of
+const arr = [1, 2, 3, 4, 5];
+for (const element of arr) {
+  console.log(element);
+}
+const greet = "Hello world";
+for (const element of greet) {
+  console.log(element);
+  console.log(greet.length);
+}
+
+/// map is nown for its uniqueness
+
+/// map is not itereable
+const map = new Map();
+map.set("Pk", "Pakistan");
+map.set("IN", "India");
+map.set("US", "United States");
+map.set("Pk", "Pakistan");
+console.log(map);
+console.log(map.length);
+console.log(typeof map);
+console.log(Object.keys(map));
+console.log(Object.values(map));
---
+++ forinloop.js
@@ -0,0 +1,35 @@
+// for in loop
+const obj = {
+  name: "Fahad",
+  age: 27,
+  height: "5.9ft",
+};
+
+for (const key in obj) {
+  console.log(key);
+}
+
+for (const key in obj) {
+  console.log(` ${key} : ${obj[key]}`);
+}
+const obj1 = ["banana", , "apple", "mangoo"];
+
+// for each
+
+// A method available only on arrays, used to run a function for each element in the array.
+obj1.forEach((items) => {
+  console.log(items);
+});
+const languages = ["Js", "python", "java"];
+
+function lang(item) {
+  console.log(item);
+}
+languages.forEach(lang);
+
+// for loop when number of iteration is known
+// while loop when number of iteration is unknown check until condition met
+// do while when you want to exceute atelease once whether condition met or not met
+// for of loop works on  Array map, string, sets iteartes overvalue
+// for in loop works on  Objects iteartes over key value pairs
+// for each loop works on  Arrays only iteartes over  value with optional index
