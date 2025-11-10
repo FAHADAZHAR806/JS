f9aa2b2ddd36257ac32e9e99840bbc8e77429db0
FADI <fahadkiyani28@gmail.com>
2025-11-10 15:05:31 +0500

    guess

---
+++ GuessNumber/guess.js
@@ -0,0 +1,5 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+const submit = document.getElementById("submit");
+const userInput = document.getElementById("guessField");
+const guessSlot = document.querySelector(".guesses");
+const Remaining = document.querySelector(".lastResult");
