be6b9b43c776f4998e3590f8e80c21b174e57936
FADI <fahadkiyani28@gmail.com>
2025-11-15 21:20:11 +0500

    Promises

---
+++ -31promise.js
@@ -0,0 +1,28 @@
+let a = 7;
+new Promise((resolve, reject) => {
+  if (a == 71) {
+    resolve("done");
+  } else {
+    reject("some thing wrong");
+  }
+})
+  .then((re) => {
+    console.log(re);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+function TimeChecking() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Display after 5 seconds");
+    }, 5000);
+  });
+}
+
+async function Display() {
+  let result = await TimeChecking();
+  console.log(result);
+}
+Display();
