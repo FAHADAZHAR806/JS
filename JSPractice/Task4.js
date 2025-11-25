189daa0403ec9226dc24536ca2f05d7bf9a4a4e4
FADI <fahadkiyani28@gmail.com>
2025-11-25 18:14:06 +0500

    task4

---
+++ JSPractice/Task4.js
@@ -0,0 +1,28 @@
+function sayHi() {
+  console.log("Hi, I am Fadi");
+}
+sayHi();
+sayHi();
+sayHi();
+function greetPerson(name) {
+  console.log("Hello", name);
+}
+greetPerson("Fahad");
+greetPerson("Fai");
+greetPerson("FahadAzhar");
+
+function addNumbers(a, b) {
+  sum = a + b;
+  return sum;
+}
+console.log(addNumbers(5, 7));
+console.log(addNumbers(6, 7));
+console.log(addNumbers(9, 7));
+
+const squareNumber = (num) => {
+  let square = num * num;
+  return square;
+};
+console.log(squareNumber(5));
+console.log(squareNumber(10));
+console.log(squareNumber(3));
