6d7d2f87743d4f15737412c35f4d79ddf06c2e39
FADI <fahadkiyani28@gmail.com>
2025-11-25 18:01:02 +0500

    task-3

---
+++ JSPractice/Task3.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
+
+let array = [5, 10, 15, 20];
+let sum = 0;
+for (let i = 0; i < array.length; i++) {
+  sum = sum + array[i];
+}
+console.log(sum);
+
+let food = ["Biryani", "Chineese", "Beaf"];
+for (let i = 0; i < food.length; i++) {
+  console.log("I love", food[i]);
+}
