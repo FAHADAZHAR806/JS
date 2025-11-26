c763f6c0095b48282f3739d0e161589f4051956b
FADI <fahadkiyani28@gmail.com>
2025-11-26 17:41:28 +0500

    Task9

---
+++ JSPractice/Task9.js
@@ -0,0 +1,37 @@
+function runTask(callback) {
+  console.log("Task Started");
+  callback();
+  console.log("Task Ended");
+}
+function done() {
+  console.log("Task finished!");
+}
+runTask(done);
+
+function myFilter(arr, callback) {
+  let result = [];
+
+  for (const element of arr) {
+    if (callback(element)) {
+      result.push(element);
+    }
+  }
+  return result;
+}
+
+function filtered(x) {
+  return x > 4;
+}
+console.log(myFilter([1, 2, 3, 4, 5, 6, 9], filtered))
+
+function delayMessage(message, callback) {
+  console.log("waiting");
+  setTimeout(() => {
+    console.log(message);
+    callback();
+  }, 3000);
+}
+function done() {
+  console.log("done");
+}
+delayMessage("Hello Fadi", done);
